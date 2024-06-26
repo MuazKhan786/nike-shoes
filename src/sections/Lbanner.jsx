df298c99095834375bf3d2d07f8f6797593e54a5
Muaz Ur Rehman <muazkhan786@gmail.com>
2024-06-27 01:31:56 +0530

    Add files via upload

---
+++ src/sections/Lbanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Lbanner = () => {
+  return (
+    <section className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+        
+        </section>
+  )
+}
+
+export default Lbanner
